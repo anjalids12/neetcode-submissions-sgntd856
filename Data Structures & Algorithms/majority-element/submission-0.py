afe400349d16c74fd3d8562706240337ef3bf7dd
Anjali Gautam <168667109+anjalids12@users.noreply.github.com>
2026-05-09 14:41:56 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #Hash_map
+        element_count = Counter(nums)
+        max_val = 0
+        max_key = 0
+        for k,v in element_count.items():
+            if v > max_val:
+                max_val = v
+                max_key = k
+        return max_key
+            
